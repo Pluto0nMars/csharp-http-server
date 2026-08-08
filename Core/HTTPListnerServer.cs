3a0af006f12475b204ec5badb334b86cc69900ad
aakin7 <aakindelen132@gmail.com>
2026-08-08 17:45:06 -0500

    handlers

--- Core/HTTPListnerServer.cs
+++ Core/HTTPListnerServer.cs
@@ -14,4 +14,39 @@ public class HttpListenerServer
     private readonly HttpListener _listener;
     private readonly CancellationTokenSource _cancellationTokenSource;
     private readonly Dictionary<string, Func<HttpListenerContext, Task>> _routes;
+
+
+    public HttpListenerServer(string [] prefixes)
+    {   
+        _listener = new HttpListener();
+        _cancellationTokenSource = new CancellationTokenSource();
+        _routes = new Dictionary<string, Func<HttpListenerContext, Task>>();
+
+
+        //add URL prefixes for the server to listen on
+        foreach (string prefix in prefixes)
+        {
+            _listener.Prefixes.Add(prefix);
+        }
+    }
+
+
+    //helper methods
+    private async Task<string> ReadRequestBody(HttpListenerRequest request)
+    {
+        if (!request.HasEntityBody)
+        {
+            return string.Empty;
+        }
+        using var reader = new StreamReader(request.InputStream, request.ContentEncoding);
+        return await reader.ReadToEndAsync();
+    }
+
+    private async Task WriteResponse(HttpListenerResponse response, string content)
+    {
+        byte[] buffer = Encoding.UTF8.GetBytes(content);
+        response.ContentLength64 = buffer.Length;
+        await response.OutputStream.WriteAsync(buffer,0, buffer.Length);
+        response.Close();
+    }
 }
